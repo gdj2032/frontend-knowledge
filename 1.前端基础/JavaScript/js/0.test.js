781d52e5995bab5b79d2df478924a596f4c07daf
顾冬杰 <djgu@tmindtech.com>
2024-11-12 21:14:33 +0800

    [Feat]:flutter基本配置

--- 1.前端基础/JavaScript/js/0.test.js
+++ 1.前端基础/JavaScript/js/0.test.js
@@ -1,22 +1,97 @@
-const obj = Object.defineProperties(
-  {},
-  {
-    a: {
-      writable: false,
-      enumerable: false,
-      configurable: false,
-      value: 1
-    },
-    b: {},
-    c: {
-      value: 3,
-      configurable: true
+function Promise1(exector) {
+  this.state = 'pending';
+  this.value = null;
+  this.callbacks = [];
+
+  const resolve = value => {
+    if (this.state !== 'pending') return;
+    this.state = 'resolved';
+    this.value = value;
+    if (this.callbacks.length > 0) {
+      setTimeout(() => {
+        this.callbacks.forEach(cb => cb.onResolved(value));
+      });
     }
+  };
+
+  const reject = reason => {
+    if (this.state !== 'pending') return;
+    this.state = 'rejected';
+    this.value = reason;
+    if (this.callbacks.length > 0) {
+      setTimeout(() => {
+        this.callbacks.forEach(cb => cb.onRejected(value));
+      });
+    }
+  };
+
+  try {
+    exector(resolve, reject);
+  } catch (error) {
+    console.info('--- error --->', error);
+  }
+}
+
+Promise1.prototype.then = function (onResolved, onRejected) {
+  const self = this;
+  onResolved = typeof onResolved === 'function' ? onResolved : v => v;
+  onRejected =
+    typeof onRejected === 'function'
+      ? onRejected
+      : r => {
+          throw r;
+        };
+  const promise = new Promise1((resolve, reject) => {
+    function handle(callback) {
+      try {
+        const x = callback(self.value);
+        if (x instanceof Promise1) {
+          x.then(resolve, reject);
+          resolve(x.value);
+        } else {
+          resolve(x);
+        }
+      } catch (error) {
+        reject(error);
+      }
+    }
+
+    if (self.state === 'pending') {
+      self.callbacks.push({
+        onResolved() {
+          handle(onResolved);
+        },
+        onRejected() {
+          handle(onRejected);
+        }
+      });
+    }
+
+    if (self.state === 'resolved') {
+      setTimeout(() => {
+        handle(onResolved);
+      });
+    }
+
+    if (self.state === 'rejected') {
+      setTimeout(() => {
+        handle(onRejected);
+      });
+    }
+  });
+  return promise;
+};
+
+const p = new Promise1((res, rej) => {
+  // res(1);
+  rej('error');
+}).then(
+  v => {
+    console.info('--- v --->', v);
+  },
+  r => {
+    console.info('--- r --->', r);
   }
 );
 
-const demo = Object.getOwnPropertyDescriptor(obj, 'c');
-
-demo.value = 333;
-
-console.info('--- demo --->', demo, Object.getOwnPropertyDescriptors(obj));
+console.info('--- p --->', p);
